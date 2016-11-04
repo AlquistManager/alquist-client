7e0b51d29132bc4c18956996c46ee3bb61bf431e
ermrk <ermrk@seznam.cz>
2016-11-04 18:41:29 +0100

    Carousel

--- script.js
+++ script.js
@@ -77,6 +77,11 @@ $(document).on("click", "#back", function (e) {
 
 });
 
+//Click on back button
+$("iframe").on("click", function (e) {
+    e.preventDefault();
+});
+
 //send message to Alquist by REST
 function sendInput(text) {
     // escape html tags
@@ -132,8 +137,8 @@ function showSystemMessages(messages, input) {
             cumulatedDelay += messages[i]['delay'];
             showIframe(messages[i]['payload']['url'], messages[i]['payload']['width'], messages[i]['payload']['height'], messages[i]['payload']['scrolling'], messages[i]['payload']['align'], cumulatedDelay);
         }
-        else if (messages[i]['type'] == "carousel"){
-            showCarousel(messages[i]['payload']['parts'], cumulatedDelay);
+        else if (messages[i]['type'] == "carousel") {
+            showCarousel(messages[i]['payload']['parts'], messages[i]['payload']['urls'], cumulatedDelay);
         }
     }
     // if there is some delay, than hide input
@@ -262,19 +267,19 @@ function showIframe(url, width, height, scrolling, align, delay) {
     }, delay);
 }
 
-function showCarousel(parts) {
-    // escape html tags
-    parts = parts.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+function showCarousel(parts, urls, delay) {
     //Show it on page
-    var carousell=$('<div class="multiple-items"> </div>');
-    var well = $('<div class="well" style="margin-bottom: 20px;"><div class="clearfix"></div></div>').append(carousell);
-    $("#communication_area").append(well);
-    for (var part in parts){
-        carousell.append("<div>"+part+"</div>");
-    }
-    //scroll to bottom of page
-    $("html, body").animate({scrollTop: $(document).height()}, scrollToBottomTime);
-    slick();
+    setTimeout(function () {
+        var carousell = $('<div class="multiple-items" style="height:600px"> </div>');
+        var well = $('<div class="well" style="margin-bottom: 20px;"><div class="clearfix"></div></div>').append(carousell);
+        $("#carousel").append(well);
+        for (var i = 0; i < parts.length; i++) {
+            carousell.append("<a style='height:600px' href='" + urls[i] + "'>" + parts[i] + "</a>");
+        }
+        //scroll to bottom of page
+        $("html, body").animate({scrollTop: $(document).height()}, scrollToBottomTime);
+        slick();
+    }, delay);
 }
 
 // callback function for button click
@@ -352,9 +357,25 @@ function inputFieldSizeHack() {
 
 function slick() {
     $('.multiple-items').slick({
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    dots: true,
-    arrows:false
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        arrows: false,
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }]
     });
 }
